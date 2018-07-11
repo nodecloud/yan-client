5ac6579d82d6dac5d5c5dcec72a046b10b6f13b3
miaowing <i@zfeng.net>
2018-07-11 15:03:31 +0800

    support patch request

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import YanClient from './annotation/YanClient';
 import GetMapping from './annotation/GetMapping';
 import PostMapping from './annotation/PostMapping';
+import PatchMapping from './annotation/PatchMapping';
 import PutMapping from './annotation/PutMapping';
 import DeleteMapping from './annotation/DeleteMapping';
 import Params from './annotation/Params';
@@ -9,4 +10,4 @@ import ResponseBody from './annotation/ResponseBody';
 import ResponseHeader from './annotation/ResponseHeader';
 
 export default YanClient;
-export {GetMapping, PostMapping, PutMapping, DeleteMapping, Params, Header, ResponseBody, ResponseHeader};
+export {GetMapping, PostMapping, PutMapping, DeleteMapping, Params, Header, ResponseBody, ResponseHeader, PatchMapping};
